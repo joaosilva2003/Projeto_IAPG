8fda58c0abd85dc9657a0d0e48a75d8ebfeac7c3
João <2023114593@ufp.edu.pt>
2024-01-07 21:36:05 +0000

    Add compilation files

---
+++ ProjetoIAPG/cmake-build-debug/CMakeFiles/ProjetoIAPG.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/joaok/Desktop/Git/ProjetoIAPG/funcoes.c" "CMakeFiles/ProjetoIAPG.dir/funcoes.c.obj" "gcc" "CMakeFiles/ProjetoIAPG.dir/funcoes.c.obj.d"
+  "C:/Users/joaok/Desktop/Git/ProjetoIAPG/main.c" "CMakeFiles/ProjetoIAPG.dir/main.c.obj" "gcc" "CMakeFiles/ProjetoIAPG.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
